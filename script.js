2e601135a44bd52ccff9727b52569ef3e45d56a1
Hanz4Me <raihanfatturrahman204@gmail.com>
2023-07-19 19:44:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+document.getElementById("btn").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+window.onload = () => {
+	let button = document.querySelector("#btn");
+
+	button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+
+	let tinggibadan = parseInt(document
+			.querySelector("#tinggibadan").value);
+
+	let beratbadan = parseInt(document
+			.querySelector("#beratbadan").value);
+
+	let result = document.querySelector("#result");
+
+	if (tinggibadan === "" || isNaN(tinggibadan))
+		result.innerHTML = "tinggi badan tidak boleh kosong";
+
+	else if (beratbadan === "" || isNaN(beratbadan))
+		result.innerHTML = "tinggi badan tidak boleh kosong";
+
+	else {
+
+		let bmi = (beratbadan / ((tinggibadan * tinggibadan)
+							/ 10000)).toFixed(2);
+
+		if (bmi < 18.6) result.innerHTML =
+			`Under beratbadan : <span>${bmi}</span>`;
+
+		else if (bmi >= 18.6 && bmi < 24.9)
+			result.innerHTML =
+				`Normal : <span>${bmi}</span>`;
+
+		else result.innerHTML =
+			`Over beratbadan : <span>${bmi}</span>`;
+	}
+}
